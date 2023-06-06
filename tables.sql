2d67f6f84d7eb9b1d9a45d00258284294afdb018
chriss1525 <okothchris15@gmail.com>
2023-06-06 23:22:28 +0300

    move .html files and .css files to respective folders

--- tables.sql
+++ tables.sql
@@ -28,3 +28,15 @@ CREATE TABLE IF NOT EXISTS Flashcards (
     deck_id INT,
     FOREIGN KEY (deck_id) REFERENCES Deck(deck_id)
 );
+CREATE TABLE IF NOT EXISTS Progress (
+    progress_id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    password_hash VARCHAR(256),
+    email VARCHAR(256),
+    last_viewed DATETIME,
+    completed BOOLEAN,
+    user_id INT,
+    flashcard_id INT,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (flashcard_id) REFERENCES Flashcards(flashcard_id)
+);
